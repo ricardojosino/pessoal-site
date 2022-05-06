00c8df188a457de009b87b7dc50461990235e49f
ricardojosino <ricardojosino@gmail.com>
2022-05-06 09:54:51 -0300

    Primeira versão do site

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    colors: {
+      'color01': '#24242B',
+      'color02': '#DEA13A',
+      'color03': '#820AD1',
+      'color04': '#333333',
+      'color05': '#CCCCCC',
+      'color06': '#EEEEEE',
+      'color07': '#FFFFFF',
+
+    },
+
+    fontFamily: {'ubuntu': 'ubuntu'},
+
+    extend: {
+
+      screens: {
+        'xl' : '1200px',
+      },
+
+    },
+  },
+  plugins: [],
+}
